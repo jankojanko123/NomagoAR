e73762ff5f5b8e3990cc501a808effca89771db1
Jan <jan.mihevc98@gmail.com>
2020-01-06 14:51:57 +0100

    demo

--- script.js
+++ script.js
@@ -9,62 +9,79 @@ function staticLoadPlaces() {
         {
             name: "TEST",
             location: {
-                lat: 46.056648, // add here latitude if using static data
-                lng: 14.500986, // add here longitude if using static data
+                lat: 46.056648,
+                lng: 14.500986,
 
             }
         },
         {
             name: "TEST 2",
             location: {
-                lat: 46.056544, // add here latitude if using static data
-                lng: 14.502188, // add here longitude if using static data
+                lat: 46.056544,
+                lng: 14.502188,
 
             }
         },
         {
             name: "TEST 3",
             location: {
-                lat: 46.056739, // add here latitude if using static data
-                lng: 14.501869, // add here longitude if using static data
+                lat: 46.056739,
+                lng: 14.501869,
 
             }
         },
         {
             name: "TEST 4",
             location: {
-                lat: 46.056272, // add here latitude if using static data
-                lng: 14.501944, // add here longitude if using static data
+                lat: 46.056272,
+                lng: 14.501944,
+
+            }
+        },
+        {
+            name: "TEST5",
+            location: {
+                lat: 46.057670,
+                lng: 14.501333,
+
+            }
+        },
+        {
+            name: "TEST6",
+            location: {
+                lat: 46.057882,
+                lng: 14.501987,
+
+            }
+        },
+        {
+            name: "TEST7",
+            location: {
+                lat: 46.057908,
+                lng: 14.501611,
 
             }
         },
     ];
 }
-
-/*
-
-*/
-
-
-
 function renderPlaces(places) {
     let scene = document.querySelector('a-scene');
 
     places.forEach((place) => {
-        let latitude = place.location.lat;
-        let longitude = place.location.lng;
+        const latitude = place.location.lat;
+        const longitude = place.location.lng;
 
-        let model = document.createElement('a-entity');
-        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-        model.setAttribute('gltf-model', './assets/magnemite/scene.gltf');
-        model.setAttribute('rotation', '0 180 0');
-        model.setAttribute('animation-mixer', '');
-        model.setAttribute('scale', '0.5 0.5 0.5');
+        // add place name
+        const text = document.createElement('a-link');
+        text.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+        text.setAttribute('title', place.name);
+        text.setAttribute('href', 'http://www.example.com/');
+        text.setAttribute('scale', '20 20 20');
 
-        model.addEventListener('loaded', () => {
+        text.addEventListener('loaded', () => {
             window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
         });
 
-        scene.appendChild(model);
+        scene.appendChild(text);
     });
 }
