fcc3b30829bb1b6388907f2b1477f4ec14173d89
Jan <jan.mihevc98@gmail.com>
2020-01-06 14:34:29 +0100

    demo

--- script.js
+++ script.js
@@ -5,51 +5,44 @@ window.onload = () => {
 
 function staticLoadPlaces() {
     return [
+
         {
-            name: 'Magnemite',
+            name: "TEST",
             location: {
-                lat: 46.056450,
-                lng: 14.502220,
+                lat: 46.056648, // add here latitude if using static data
+                lng: 14.500986, // add here longitude if using static data
+
+            }
+        },
+        {
+            name: "TEST 2",
+            location: {
+                lat: 46.056544, // add here latitude if using static data
+                lng: 14.502188, // add here longitude if using static data
+
+            }
+        },
+        {
+            name: "TEST 3",
+            location: {
+                lat: 46.056739, // add here latitude if using static data
+                lng: 14.501869, // add here longitude if using static data
+
+            }
+        },
+        {
+            name: "TEST 4",
+            location: {
+                lat: 46.056272, // add here latitude if using static data
+                lng: 14.501944, // add here longitude if using static data
+
             }
         },
     ];
 }
 
 /*
-const PLACES = [
-    {
-        name: "TEST",
-        location: {
-            lat: 46.056648, // add here latitude if using static data
-            lng: 14.500986, // add here longitude if using static data
 
-        }
-    },
-    {
-        name: "TEST 2",
-        location: {
-            lat: 46.056544, // add here latitude if using static data
-            lng: 14.502188, // add here longitude if using static data
-
-        }
-    },
-    {
-        name: "TEST 3",
-        location: {
-            lat: 46.056739, // add here latitude if using static data
-            lng: 14.501869, // add here longitude if using static data
-
-        }
-    },
-    {
-        name: "TEST 4",
-        location: {
-            lat: 46.056272, // add here latitude if using static data
-            lng: 14.501944, // add here longitude if using static data
-
-        }
-    },
-];
 */
 
 
