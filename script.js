c5b71a73e9173a5be5176229b0b427b239904aee
Jan <jan.mihevc98@gmail.com>
2020-01-07 15:25:30 +0100

    bus station png

--- script.js
+++ script.js
@@ -131,7 +131,7 @@ function renderPlaces(places) {
         const icon = document.createElement('a-image');
         icon.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
         icon.setAttribute('name', place.name);
-        icon.setAttribute('src', 'https://image.flaticon.com/icons/svg/526/526763.svg');
+        icon.setAttribute('src', 'https://image.flaticon.com/icons/svg/1042/1042263.svg');
 
         // for debug purposes, just show in a bigger scale, otherwise I have to personally go on places...
         icon.setAttribute('scale', '10, 10');
